7ffa54f5060540746898ebfc6ef5bd433b33953c
flvSantos15 <flvsantos300@gmail.com>
2021-08-10 22:16:23 -0300

    Utilizando MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return('PAREI NOS 05:00 DO VIDEO CONFIGURANDO MIRAGE')
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
